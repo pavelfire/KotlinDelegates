741d01f54d039b429469121cba90203cc9cae47a
Pavel Dolganov mSt <fire_ra@bk.ru>
2023-11-09 09:32:13 +0300

    lazy delegate example

--- app/src/main/java/com/vk/directop/kotlindelegates/MainActivity.kt
+++ app/src/main/java/com/vk/directop/kotlindelegates/MainActivity.kt
@@ -3,21 +3,41 @@ package com.vk.directop.kotlindelegates
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.reflect.KProperty
 
 class MainActivity : AppCompatActivity(),
     AnalyticsLogger by AnalyticsLoggerImpl(),
     DeepLinkHandler by DeepLinkHandlerImpl() {
 
+    private val myObj by lazy{
+        println("Hello world")
+        5
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         registerLifecycleOwner(this)
         setContentView(R.layout.activity_main)
+
+        println(myObj)
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         handleDeepLink(this, intent)
     }
+}
 
+class MyLazy<out T: Any>(
+    private val myInitiaize: () -> T
+){
+    private var value: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>):T{
+        return if(value == null){
+            value = myInitiaize()
+            value!!
+        }else value!!
+    }
 }
 
